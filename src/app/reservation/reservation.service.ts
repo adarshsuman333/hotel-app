240728b548a91fd00e605522fd59e4fb48781992
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-01 03:13:01 +0530

    Creating the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  //CRUD operations
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    if (index >= 0 && index < this.reservations.length)
+      this.reservations[index] = updatedReservation;
+    
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    if (index >= 0 && index < this.reservations.length) {
+      this.reservations.splice(index, 1);
+    }
+  }
 }
