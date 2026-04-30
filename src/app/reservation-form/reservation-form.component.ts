d7390ba25463a63d434f7667a84e943bef776885
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-01 02:43:59 +0530

    Validating a form using reactive form validation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -7,12 +7,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {
+    this.reservationForm = this.formBuilder.group({
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      roomNumber: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.reservationForm = this.formBuilder.group({
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      roomNumber: ['', Validators.required]
+    });
+  }
+
   onSubmit() {
-    
+    if (this.reservationForm.valid) {
+      console.log("Valid");
+    } else {
+      console.log("Form is invalid");
+    }
   }
 
 }
