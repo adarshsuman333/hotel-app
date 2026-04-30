6639e5a6fd08064198adf43af98cf25526378cd5
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-01 02:26:05 +0530

    Creating the reservation form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -9,4 +9,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservationFormComponent {
 
+  reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit() {
+    
+  }
+
 }
--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
-
+import { RouterModule } from '@angular/router'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,10 @@ import { ReservationListComponent } from '../reservation-list/reservation-list.c
     ReservationListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
