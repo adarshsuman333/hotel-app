8af410f3df5d02b05fe1341e3fdb6ce72f9ebdcf
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-04 09:22:02 +0530

    Adding a delete button to the reservation to delete the reservation

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -15,4 +15,10 @@ export class ReservationListComponent implements OnInit {
   ngOnInit(): void {
     this.reservations = this.reservationService.getReservations();
   }
+
+  deleteReservation(id: string) {
+    this.reservationService.deleteReservation(id);
+    this.reservations = this.reservationService.getReservations();
+  }
+
 }
